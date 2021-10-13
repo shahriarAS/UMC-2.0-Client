ed20d0ade328539c4d5366a2b7a5b99aa88c5758
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2021-10-13 17:40:40 +0600

    Fixed

--- src/Components/pages/CoursePlayer/EmbdedPlayer.jsx
+++ src/Components/pages/CoursePlayer/EmbdedPlayer.jsx
@@ -5,7 +5,10 @@ function EmbdedPlayer(props) {
     return (
         <>
             <h2 className="text-xl mb-4 font-bold text-center text-white">{classTitle}</h2>
-            <div style={{ "padding": "66.67% 0 0 0", "position": "relative" }}> <iframe src={`https://player.vimeo.com/video/${videoLink}?h=6e6dd591b7&title=0&byline=0&portrait=0`} style={{ "position": "absolute", "top": "0", "left": "0", "width": "100%", "height": "100%" }} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            <div style={{ "padding": "66.67% 0 0 0", "position": "relative" }}>
+                <iframe src={`https://player.vimeo.com/video/${videoLink}?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;h=4d8c901d47`} style={{ "position": "absolute", "top": "0", "left": "0", "width": "100%", "height": "100%" }} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
+
+                </iframe>
             </div><script src="https://player.vimeo.com/api/player.js"></script>
         </>
     )
